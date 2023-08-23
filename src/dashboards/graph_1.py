6bea71b7400f4f41b69731ab58e541ab0b5adebf
karts001 <karts001@gmail.com>
2023-08-23 22:58:29 +0100

    plotly dash integrated with fast api
    plotly dashboard integrated with fast api server

---
+++ src/dashboards/graph_1.py
@@ -0,0 +1,39 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+
+from src.file_paths import get_player_weekly_score_csv_path
+import src.common.pandas_methods as pdm
+
+file_path = get_player_weekly_score_csv_path()
+df = pdm.convert_data_to_dataframe(file_path)
+
+
+
+app = Dash(__name__,
+           requests_pathname_prefix="/graph_1/")
+
+app.layout = html.Div([
+    html.H1(children="Player FPL Score", style={'textAlign':'center'}),
+    dcc.Dropdown(
+        options=[
+            {"label": "Goalkeeper", "value": 1},
+            {"label": "Defender", "value": 2},
+            {"label": "Midfielder", "value": 2},
+            {"label": "Attacker", "value": 4}],
+            value=4,
+            id="dropdown-selection"),
+    dcc.Graph(id="graph-content"),
+])
+
+@callback(
+    Output("graph-content", "figure"),
+    Input("dropdown-selection", "value")
+)
+def create_graph(value):
+    dff = df[df.element_type==value]
+    scatter_graph = px.scatter(dff, y="X-Rating", x="FPL Weekly Score", color="element_type", hover_name="Full Name", size="X-Rating")
+    return scatter_graph
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+import uvicorn
+
 from src.weekly_calculation.current_team import rank_current_squad
+from src.dashboards.graph_1 import app as graph_1
 
 app = FastAPI()
+app.mount("/graph_1", WSGIMiddleware(graph_1.server))
 
-@app.get("/")
-def root():
-    potential_transfers = rank_current_squad()
-    return potential_transfers
+# @app.get("/")
+# def root():
+#     potential_transfers = rank_current_squad()
+#     return potential_transfers
     
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
